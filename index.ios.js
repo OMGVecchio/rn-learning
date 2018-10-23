023b18b4d84865b7ba162cb2c988820b0cf5f0ee
vecchio <weichao@zbj.com>
2018-10-23 18:46:26 +0800

    add navigation

--- index.ios.js
+++ index.ios.js
@@ -8,28 +8,41 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  View,
   Text,
-  View
+  FlatList
 } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+import PicturePage from './container/picture'
 
 export default class learning extends Component {
+  state = {
+    dataList: [{
+      key: '图片',
+      page: 'PicturePage'
+    }]
+  }
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <FlatList
+          data={this.state.dataList}
+          renderItem={({ item }) => <Text onPress={() => navigate(item.page)} key={item.page}>{item.key}</Text>} />
       </View>
     );
   }
 }
+
+const App = createStackNavigator({
+  Home: {
+    screen: learning
+  },
+  PicturePage: {
+    screen: PicturePage
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -37,17 +50,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
-AppRegistry.registerComponent('learning', () => learning);
+AppRegistry.registerComponent('learning', () => App);
