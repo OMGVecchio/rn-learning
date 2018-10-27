349913f241ac4aa72eaa2437e479805711eba2c4
vecchio <weichao@zbj.com>
2018-10-27 14:36:27 +0800

    try components

--- container/api.js
+++ container/api.js
@@ -15,7 +15,7 @@ import {
   // BackAndroid,
   BackHandler,
   // CameraRoll, // 需链接原生 RCTCameraRoll 库，且无相册界面
-  // Clipboard,
+  Clipboard,
   // DatePickerAndroid,
   Dimensions,
   // Easing,
@@ -27,13 +27,13 @@ import {
   Keyboard,
   // LayoutAnimation,
   // Linking,
-  // NetInfo,
+  NetInfo,
   // PanResponder,
   // PermissionsAndroid,
   // PixelRatio,
   // PushNotificationIOS,
   // Settings,
-  // Share,
+  Share,
   // Systrace,
   // TimePickerAndroid,
   // ToastAndroid,
@@ -53,6 +53,7 @@ export default class InputPage extends PureComponent {
   state = {
     storageCount: 0,
     appStateCount: 0,
+    clipboard: '',
     canGoBack: false
   }
   componentWillMount = async () => {
@@ -68,12 +69,16 @@ export default class InputPage extends PureComponent {
     BackHandler.addEventListener('hardwareBackPress', this.handleAppBack)
     this.keyboardShow = Keyboard.addListener('keyboardDidShow', () => Alert.alert('键盘展示出来', '可以了'))
     this.keyboardHide = Keyboard.addListener('keyboardDidHide', () => Alert.alert('键盘被隐藏', '关掉吧'))
+    NetInfo.addEventListener('connectionChange', this.handleNetTypeChange)
+    NetInfo.isConnected.addEventListener('connectionChange', this.handleNetStatusChange)
   }
   componentWillUnmount = () => {
     AppState.removeEventListener('change', this.handleAppStateChange)
     BackHandler.removeEventListener('hardwareBackPress', this.handleAppBack)
     this.keyboardShow.remove()
     this.keyboardHide.remove()
+    NetInfo.removeEventListener('connectionChange', this.handleNetChange)
+    NetInfo.isConnected.removeEventListener('connectionChange', this.handleNetStatusChange)
   }
   storageUpdate = async () => {
     const newCount = this.state.storageCount + 1
@@ -96,6 +101,13 @@ export default class InputPage extends PureComponent {
     }
     return false
   }
+  paste = () => {
+    Clipboard.getString().then(clipboard => {
+      this.setState({
+        clipboard
+      })
+    })
+  }
   getScreenInfo = () => {
     const info = Dimensions.get('window')
     Alert.alert('屏幕信息', JSON.stringify(info, null, 4))
@@ -103,6 +115,23 @@ export default class InputPage extends PureComponent {
   getLocationInfo = () => {
     navigator.geolocation.getCurrentPosition(data => {
       Alert.alert('当前位置信息', JSON.stringify(data, null, 4))
+    })
+  }
+  handleNetTypeChange = (connectionInfo) => {
+    Alert.alert('网络类型', connectionInfo)
+  }
+  handleNetStatusChange = (isConnected) => {
+    Alert.alert('网络状态', isConnected ? 'online' : 'offline')
+  }
+  getNetInfo = () => {
+    // NetInfo.getConnectionInfo 方法废弃了？
+    NetInfo.fetch().then(connectionInfo => Alert.alert('网络状况', connectionInfo))
+  }
+  share = () => {
+    Share.share({
+      message: '哈哈',
+      title: 'Android?',
+      uri: 'https://zhitou.zbj.com'
     })
   }
   vibrate = () => {
@@ -113,9 +142,12 @@ export default class InputPage extends PureComponent {
       <ScrollView style={styles.main}>
         <Text onPress={this.storageUpdate}>【AsyncStorage】点击更新持久数据 count => count：{this.state.storageCount}</Text>
         <Text>【AppState】在后台去的次数：{this.state.appStateCount}</Text>
+        <Text onPress={this.paste}>【Clipboard】点击粘贴：{this.state.clipboard || '???'}</Text>
         <Text onPress={this.getScreenInfo}>【Dimensions】获取屏幕信息</Text>
         <Text onPress={this.getLocationInfo}>【Geolocation】获取地理位置信息</Text>
         <TextInput placeholder="【Keyboard】测试键盘事件" />
+        <Text onPress={this.getNetInfo}>【Netinfo】获取网络状况</Text>
+        <Text onPress={this.share}>【Share】分享</Text>
         <Text onPress={this.vibrate}>【Vibration】震动</Text>
       </ScrollView>
     )
